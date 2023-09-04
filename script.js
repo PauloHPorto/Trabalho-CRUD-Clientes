4e86da682922759fa1d807e1fbd88f81cf53fa3c
Paulo Henrique Porto <127361154+PauloHPorto@users.noreply.github.com>
2023-09-04 15:44:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,107 +10,70 @@ let itens;
 let id;
 
 const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? {};
-const getItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens)) ?? {};
-
-function openModal(edit = false, index = 0) {
-  modal.classList.add('active');
-
-  modal.onclick = e => {
-    if (e.target.className.indexOf("modal-container") !== -1) {
-      modal.classList.remove('active');
-    }
-  };
-
-  if (edit) {
-    sNome.value = itens[index].nome;
-    sTelefone.value = itens[index].telefone;
-    sEmail.value = itens[index].email; // Correção aqui
-    sCpf.value = itens[index].cpf;
-    id = index;
-  } else {
-    sNome.value = "";
-    sTelefone.value = "";
-    sEmail.value = "";
-    sCpf.value = "";
-  }
-}
-
-function editItem(index) {
-  openModal(true, index);
-}
-
-function deleteItem(index) {
-  itens.splice(index, 1);
-  setItensBD();
-  loadItens();
-}
-
-function insertItem(item, index) {
-  let tr = document.createElement("tr");
-
-  tr.innerHTML = `
-    <td>${item.nome}</td>
-    <td>${item.telefone}</td>
-    <td>R$ ${item.cpf}</td>
-    <td>${item.email}</td>
-
-    <td class="acao">
-      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
-    </td>
-    <td class="acao">
-      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
-    </td>
-  `;
-  tbody.appendChild(tr);
-}
-
-btnSalvar.onclick = (e) => {
-  if (
-    sNome.value === "" ||
-    sTelefone.value === "" ||
-    sEmail.value === "" ||
-    sCpf.value === ""
-  ) {
-    return;
-  }
-
-  e.preventDefault();
-
-  if (id !== undefined) {
-    itens[id].nome = sNome.value;
-    itens[id].telefone = sTelefone.value;
-    itens[id].email = sEmail.value;
-    itens[id].cpf = sCpf.value;
-  } else {
-    itens.push({
-      nome: sNome.value,
-      telefone: sTelefone.value, // Correção aqui
-      email: sEmail.value, // Correção aqui
-      cpf: sCpf.value,
-    });
-  }
-
-  setItensBD();
-
-  modal.classList.remove("active");
-  loadItens();
-  id = undefined;
-};
-
 function getItensBD() {
-  return JSON.parse(localStorage.getItem("dbfunc")) || [];
-}
-
-function setItensBD() {
-  localStorage.setItem("dbfunc", JSON.stringify(itens));
+  return localStorage.setItem('dbfunc', JSON.stringify(itens)) ?? {};
 }
 
 function loadItens() {
-  itens = getItensBD();
-  tbody.innerHTML = "";
+  itens = getItensBD ()
+  tbody.innerHTML = ''
   itens.forEach((item, index) => {
-    insertItem(item, index);
-  });
+    insertItem (item, idenx)
+  })
 }
 
 loadItens();
+
+function insertItem(item, index){
+  let tr = document.createElement('tr')
+
+  tr.innerHTML = `
+  <td>${item.nome}</td>
+  <td>${item.telefone}</td>
+  <td>${item.email}</td>
+  <td>${item.cpf}</td>
+
+  <td class= "acao">
+    
+    <button onclick="editItem(${index})"><i class= 'bx bx-edit' ></i></button>
+    </td>
+    <td class= "acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+    `
+    tbody.appendChild(tr)
+}
+
+function editItem(index){
+
+  openModal(true, index)
+}
+
+function deleteItem(idenx){
+  itens.splice(index, 1)
+  setItensBD()
+  loadItens()
+}
+
+function openModal(edit = false, index = 0){
+  modal.classList.add('aactive')
+
+  modal.onclick = e => {
+    if (e.target,className.indexOf('modal-container') !== -1 ){}
+      modal.classList.remove('active')
+  }
+}
+
+if (edit) {
+  sNome.value = itens[index].nome
+  sTelefone.value = itens [index].telefone
+  sEmail.value = itens [index].emal
+  sCpf.value = itens [index].cpf
+  id = index
+} else {
+  sNome.value = ''
+  sTelefone.value = ''
+  sEmail.value = ''
+  sCpf.value.value = ''
+}
+
