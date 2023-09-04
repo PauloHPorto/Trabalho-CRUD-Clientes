bb7d826fedb603f5a95e5f44aa4137a0508f84da
Paulo Henrique Porto <127361154+PauloHPorto@users.noreply.github.com>
2023-09-04 14:33:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ let itens;
 let id;
 
 function openModal(edit = false, index = 0) {
-  modal.classList.add("active");
+  modal.classList.add('active');
 
-  modal.onclick = (e) => {
+  modal.onclick = e => {
     if (e.target.className.indexOf("modal-container") !== -1) {
-      modal.classList.remove("active");
+      modal.classList.remove('active');
     }
   };
 
