287cfb65e6b01415d96d0ecc6f381513ed498785
Paulo Henrique Porto <127361154+PauloHPorto@users.noreply.github.com>
2023-09-13 14:56:33 -0300

    Finalizado

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function clearFields(){
   document.querySelector("#Telefone").value = "";
 }
 
+//Adicionar Cliente
 document.querySelector("#client-form").addEventListener("submit",(e) =>{
   e.preventDefault();
 
@@ -48,9 +49,35 @@ document.querySelector("#client-form").addEventListener("submit",(e) =>{
     showAlert("Cliente Adicionado", "success")
 
     }
+    else{
+      selectedRow.appendChildren[0].textContent = primeiroNome
+      selectedRow.appendChildren[1].textContent = sobrenome
+      selectedRow.appendChildren[2].textContent = cpf
+      selectedRow.appendChildren[3].textContent = telefone
+      selectedRow = null;
+      showAlert("Infomações do Cliente Editadas", "info");
+    }
+
+    clearFields();
+
   }
 });
 
+//Editar informações
+document.querySelector("#client-list").addEventListener("click", (e) =>{
+  target = e.target;
+  if(target.classList.contains("edit")){
+      linhaSelecionada = target.parentElement.parentElement;
+      document.querySelector("#primeiroNome").value = linhaSelecionada.children[0].textContent;
+      document.querySelector("#Sobrenome").value = linhaSelecionada.children[1].textContent;
+      document.querySelector("#Cpf").value = linhaSelecionada.children[2].textContent;
+      document.querySelector("#Telefone").value = linhaSelecionada.children[3].textContent;
+  }
+});
+
+
+
+//Deletar Cliente
 document.querySelector("#client-list").addEventListener("click", (e) =>{target = e.target;
   if(target.classList.contains("delete")){
       target.parentElement.parentElement.remove();
